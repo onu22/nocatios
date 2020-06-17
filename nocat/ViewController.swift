8b2df9eb294bff372acb44f5d89a22d442698e04
onuorah pascal <microsoft2b@yahoo.com>
2020-06-17 13:32:44 +0100

    added table view to hold list of returned users

--- nocat/ViewController.swift
+++ nocat/ViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
 
+    
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+           cell.textLabel?.text = dailyTasks[indexPath.row]
+            return cell
+        }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return dailyTasks.count
+    }
+    
+    //Data source methods
+       //  Create [String] arrays of tasks
+       let dailyTasks = ["windows",
+                         "doors",
+                         "boiler?",
+                         "mailbox",
+                         "containers",
+                         "pipes",
+                         "occurrences",]
+       
+         
+
+    @IBAction func refreshUsers(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
